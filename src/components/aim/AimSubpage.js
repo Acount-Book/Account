1e62f1fca48da9e05d4f606082b72e4c596f1124
Leehyunbum6792 <142865344+Leehyunbum6792@users.noreply.github.com>
2023-12-18 15:36:06 +0900

    Add files via upload

--- src/components/aim/AimSubpage.js
+++ src/components/aim/AimSubpage.js
@@ -4,6 +4,7 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { GoalAmountContext } from "../../context/GoalAmoutContext";
 import { useGoalContext } from "../../context/GoalContext";
+import AimSub from "../../assets/style/AimSub.module.css";
 
 const AimSubPage = () => {
   const { setGoalAmount } = useContext(GoalAmountContext);
@@ -25,38 +26,36 @@ const AimSubPage = () => {
     });
   };
 
-  const generateDateRange = () => {
-    const startMonth = startDate.toLocaleString("default", { month: "long" });
-    const startDay = startDate.getDate();
-    const endMonth = endDate.toLocaleString("default", { month: "long" });
-    const endDay = endDate.getDate();
-
-    return `${startMonth} ${startDay} - ${endMonth} ${endDay}`;
-  };
-
   return (
     <div>
-      <h1>목표 설정</h1>
+      <h1 className={AimSub.title}>목표 설정</h1>
 
       <div>
-        <form>
-          목표 금액&nbsp;&nbsp;
-          <input type="number" value={amount} onChange={handleAmountChange} />
+        <form className={AimSub.money}>
+          <p className={AimSub.titlemoney}> 목표 금액&nbsp;&nbsp;</p>
+          <input
+            type="number"
+            value={amount}
+            onChange={handleAmountChange}
+            className={AimSub.input}
+          />
         </form>
-        <div>
-          <p>시작 기간</p>
+        <div className={AimSub.money}>
+          <p className={AimSub.titlemoney}>시작 기간</p>
           <input
             type="date"
             value={startDate.toISOString().substr(0, 10)}
             onChange={(event) => setStartDate(new Date(event.target.value))}
+            className={AimSub.Calendar}
           />
         </div>
-        <div>
-          <p>종료 기간</p>
+        <div className={AimSub.money}>
+          <p className={AimSub.titlemoney}>종료 기간</p>
           <input
             type="date"
             value={endDate.toISOString().substr(0, 10)}
             onChange={(event) => setEndDate(new Date(event.target.value))}
+            className={AimSub.Calendar}
           />
         </div>
       </div>
