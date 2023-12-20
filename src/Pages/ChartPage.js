2364309dea37943383f6a8f519721b6b539fd5b7
Leehyunbum6792 <142865344+Leehyunbum6792@users.noreply.github.com>
2023-12-20 15:10:13 +0900

    Add files via upload

--- src/Pages/ChartPage.js
+++ src/Pages/ChartPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Bar } from "react-chartjs-2";
+import { Bar, Doughnut } from "react-chartjs-2";
 import { useExpenseContext } from "../context/ExpenseContext";
 import {
   LineChart,
@@ -13,58 +13,68 @@ import {
 } from "recharts";
 import Chart from "chart.js/auto";
 import chartStyle from "../assets/style/Chart.module.css";
+
 const ChartPage = () => {
-  const {
-    istoggle,
-    setIstoggle,
-    expenses,
-    setExpenses,
-    selectedMonth,
-    setSelectedMonth,
-    handleAddExpense,
-    filterExpensesByMonth,
-    groupedExpenses,
-    groupExpensesByDate,
-    filteredExpenses,
-    generateChartData,
-    groupedExpensesByCategory,
-    accountdata,
-    categoryData,
-  } = useExpenseContext();
+  const { accountdata, categoryData } = useExpenseContext();
+
+  const decemberData = accountdata.map((data) => ({
+    date: data.date,
+    total: data.total,
+  }));
+
+  const barChartOptions = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          precision: 0,
+        },
+      },
+    },
+  };
+
+  const categoryTotal = categoryData.map((data) => data.total);
+  const categoryLabels = categoryData.map((data) => data.category);
 
   return (
     <>
       <h2 className={chartStyle.december}>12월 통계</h2>
       <div className={chartStyle.chartContainer}>
-        <LineChart
-          width={450}
-          height={250}
-          data={accountdata}
-          margin={{ top: 40, right: 20, left: 20, bottom: 0 }}>
-          <XAxis dataKey="date" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="total" stroke="#8884d8" />
-        </LineChart>
+        <Bar
+          data={{
+            labels: decemberData.map((data) => data.date),
+            datasets: [
+              {
+                label: "지출",
+                data: decemberData.map((data) => data.total),
+                backgroundColor: "#8884d8",
+                barThickness: 30,
+              },
+            ],
+          }}
+          options={barChartOptions}
+          height={180}
+          width={300}
+        />
       </div>
 
       <h3>카테고리별 지출</h3>
       <div className={chartStyle.chartContainer}>
-        <LineChart
-          width={450}
-          height={250}
-          data={categoryData}
-          margin={{ top: 20, right: 20, left: 20, bottom: 0 }}>
-          <XAxis dataKey="category" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Legend />
-          {/* Assuming you have 'total' as the property for category-wise expense */}
-          <Line dataKey="total" fill="#82ca9d" />
-        </LineChart>
+        <Doughnut
+          data={{
+            labels: categoryLabels,
+            datasets: [
+              {
+                data: categoryTotal,
+                backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#000"],
+              },
+            ],
+          }}
+          options={{
+            responsive: true,
+            maintainAspectRatio: false,
+          }}
+        />
       </div>
     </>
   );
