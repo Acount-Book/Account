13be98f061bbfbdad9a4285e9d012fde9ecd8834
kimks1234 <bisuflash1@naver.com>
2023-12-20 17:56:07 +0900

    build

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { GoalProvider } from "./context/GoalContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AuthProvider>
         <ExpenseProvider>
           <GoalProvider>
